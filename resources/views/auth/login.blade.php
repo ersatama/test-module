81b46b13b1aec7b3402d7bc97906f2eaec3046c9
Ersayin <ersatama@gmail.com>
2020-02-28 18:01:22 +0600

    not ready cabinet

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,61 +3,26 @@
 <div class="container" style="padding: 100px 0 100px 0;">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+            <div class="row login-card">
+                <div class="col-6 login-col">
+                    <div class="form-register">
+                        <div class="form-register-bg"></div>
+                        <div class="form-register-item">
+                            <div class="form-register-title">Добро пожаловать в Spark Logistics!</div>
+                            <div class="form-register-desc">Мы доставим что угодно, куда угодно. </div>
+                            <button class="form-register-button">Создать Аккаунт</button>
                         </div>
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                </div>
+                <div class="col-6 login-col">
+                    <div class="form-login">
+                        <div class="form-login-item">
+                            <div class="form-login-title">Войти в Spark Logistics!</div>
+                            <input type="text" class="form-data-input">
+                            <input type="password" class="form-data-input">
+                            <button class="login-button">Войти в кабинет</button>
                         </div>
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,13 +10,16 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 
     <!-- Font awesome-->
-    <script src="https://kit.fontawesome.com/7b9e63c713.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7b9e63c713.js" crossorigin="anonymous">
+
+    <script src="jquery.js" type="text/javascript"></script>
+    <script src="jquery.maskedinput.js" type="text/javascript"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light menu">
